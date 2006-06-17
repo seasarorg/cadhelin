d1db2d34bdcc3e49eff83b3ecf3e418dce4ee5d1
keizou <keizou@148d6ce2-a51f-4f2e-b18b-c64183d5f196>
2006-06-17 16:03:57 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/cadhelin/trunk/cadhelin-blank@24 148d6ce2-a51f-4f2e-b18b-c64183d5f196

--- WEB-INF/src/examples/controller/HelloControllerImpl.java
+++ WEB-INF/src/examples/controller/HelloControllerImpl.java
@@ -1,11 +1,9 @@
 package examples.controller;
 
-import org.seasar.cadhelin.Action;
 
 public class HelloControllerImpl {
 	
-	@Action
-	public String hellow(){
+	public String showHellow(){
 		return "hello";
 	}
 	
