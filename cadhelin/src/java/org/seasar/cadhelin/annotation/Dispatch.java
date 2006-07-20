56de50fffc5f2ae0825bcc39259e73fa44c41baf
keizou <keizou@148d6ce2-a51f-4f2e-b18b-c64183d5f196>
2006-07-20 11:23:59 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/cadhelin/trunk@88 148d6ce2-a51f-4f2e-b18b-c64183d5f196

---
+++ cadhelin/src/java/org/seasar/cadhelin/annotation/Dispatch.java
@@ -0,0 +1,15 @@
+package org.seasar.cadhelin.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dispatch {
+	String actionName();
+	String key();
+}
